432706dff5b7d46c69c01fc69a0d78b2f77ba402
Ishak96 <ishakayad96@outlook.fr>
2019-02-10 12:53:11 +0100

    create partition file ./bin/fmang [-p] [dir] OR nothig to create in the current directory

--- src/main.c
+++ src/main.c
@@ -4,13 +4,44 @@
  * @author AYAD Ishak
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 #include <manipulation_partition.h>
+
 /**
- * @author djahid abdelmoumene
+* @author ABDELMOUMENE Djahid 
+* @author AYAD Ishak
+* @brief misuse of agrs
+* @param prog name of the programme 
+*/
+static void	usage(char* prog){
+  fprintf(stderr, "usage: %s [-p arg]\n", prog);
+  exit(1);
+}
+/**
+ * @author ABDELMOUMENE Djahid 
+ * @author AYAD Ishak
  * @brief main function
  */
-
 int main(int argc, char** argv) {
-	
+ int opt;
+ char cwd[4012];
+
+ 	if(argc >= 2)
+	 	while((opt = getopt(argc, argv, "p:")) != -1){
+	 		switch(opt){
+	 			case 'p':
+	 				create_partition(optarg);
+	 				break;
+	 			default:
+	 				usage(argv[0]);
+	 				break;
+	 		}
+	 	}
+	else{
+		getcwd(cwd, sizeof(cwd));
+		create_partition(cwd);
+	}
 	return 0;
 }
